bd0d1ba52c0aa1c27aef211742404361f969139b
pirangaba <pirangabaneto@gmail.com>
2025-02-10 10:36:26 -0300

    altera o front da tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-guest-layout>  
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -12,8 +12,18 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Forgot Password -->
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="text-sm text-[#F8AD15] dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+        </div>
+        
         <!-- Password -->
-        <div class="mt-4">
+        <div>
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -24,24 +34,20 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
+        <!-- button -->
+        <div class="flex items-center justify-end mt-8">
+            <x-primary-button class="w-full justify-center h-12 !bg-[#F8AD15]">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <!-- Texto de Sign UP-->
+        <div class="w-full text-center mt-8">
+            <p>
+                You dont Have an Account?
+                <a href="{{ route('register') }}" class="text-[#F8AD15] underline font-bold">Sign Up</a>
+            </p>
+        </div>
+
     </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,16 +15,44 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+        <div class="grid grid-cols-1 md:grid-cols-2 min-h-screen sm:justify-center items-center bg-gray-100 dark:bg-gray-900 relative">
+    
+            <!-- conteúdo principal -->
+            <div class="w-full h-full px-6 py-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                <div class="w-full text-center mt-12">
+                    <h1 class="text-3xl font-bold text-[#034B5E] pb-20 md:mt-40">
+                        Hi, welcome To
+                        <span class="text-[#F8AD15]">PirasWallet</span>
+                    </h1>
+                </div>
+                <div class="w-2/3 mx-auto">
+                    {{ $slot }}
+                </div>
             </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+        
+            <div class="hidden md:flex w-full h-screen flex flex-col justify-center bg-[#034B5E] py-6">
+                <div class="w-full text-center text-white text-3xl font-bold mt-12">
+                    <h1>
+                        More Than a Wallet, <br> Your Financial World
+                    </h1>
+                </div>
+            
+                <!-- fundo verde à direita -->
+                <div class="flex justify-center items-center flex-grow">
+                    <img src="{{ asset('images/phones.png') }}" alt="phones" class="w-192">
+                </div>
+            
+                <div class="absolute bottom-4 right-4">
+                    <img src="{{ asset('images/white-logo.png') }}" alt="Logo" class="w-48">
+                </div>
             </div>
+        
+            <!-- barra inferior verde quando small -->
+            <div class="block md:hidden w-full text-center text-white bg-[#034B5E] py-2 absolute bottom-0 left-0">
+                <img src="{{ asset('images/white-logo.png') }}" alt="Logo" class="w-24 justify-self-end mr-4">
+            </div>
+        
         </div>
+        
     </body>
 </html>
